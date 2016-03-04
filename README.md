cc271e569b2e7a6f2b66500e080e842005cb8154
Jan Koppe <jan.koppe@wwu.de>
2016-03-04 09:18:29 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bagit
 A npm module to support creation, modification and validation of bagit containers
+
+This module is still in development and will probably change a lot. I suggest you come back at a later time, if you want to use it.
---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ *  A module to support creation, modification and validation of node modules.
+ */
+
+
+/**
+ *  @constructor
+ *  @param {string} directory - Initial directory of the BagIt container.
+ */
+function BagIt(directory) {
+
+}
